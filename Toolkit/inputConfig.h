40692362aed1a14e7eeb256451708c7fd5280f52
[Xiao <[xwang241@uic.edu]>
2016-12-19 17:35:28 -0600

    developing trk correction tool

--- Toolkit/inputConfig.h
+++ Toolkit/inputConfig.h
@@ -9,8 +9,55 @@
 class inputConfig{
 
 	public: 
-		inputConfig(TTree* t);
+		inputConfig(inputTree* t);
 
+		void getEntry(Long64_t jentry);
 		int trkQualityCuts(int jtrk);
 		int evtSelection(Long64_t jentry);
+		float getVzWeight(float vz);
+
+	private: 
+		const int ncentbin = 50;
+		const int nptbin  = 10;
+		inputTree * tt;
+		int    *centbin;
+		float  *  ptbin;
+		TString* ptbintitle;
 };
+
+inputConfig::inputConfig(inputTree*t){
+	tt=t;
+	int centBin[ncentbin];
+	float ptBin[nptbin];
+	centbin = centBin;
+	ptbin = ptBin;
+	
+}
+
+void inputConfig::getEntry(Long64_t jentry){
+	tt->GetEntry(jentry);
+	return;
+}
+
+int inputConfig::trkQualityCuts(int jtrk){
+	if (tt->highPurity->at(jtrk)){
+		float Et = (tt->pfHcal->at(jtrk)+tt->pfEcal->at(jtrk))/TMath::CosH(tt->trkEta->at(jtrk));
+		if(!(tt->trkPt->at(jtrk)<20 || (Et>0.5*tt->trkPt->at(jtrk)))) return 1;
+		else if ( abs(tt->trkDz->at(jtrk)/tt->trkDzError->at(jtrk)) >=3) return 1;
+		else if ( abs(tt->trkDxy->at(jtrk)/tt->trkDxyError->at(jtrk)) >=3) return 1;
+		else if ( tt->trkPtError->at(jtrk)/tt->trkPt->at(jtrk)>=0.1) return 1;
+		else if ( tt->trkNHit->at(jtrk)< 11) return 1;
+		else if ( (float)tt->trkChi2->at(jtrk)/(float)tt->trkNdof->at(jtrk)/(float)tt->trkNlayer->at(jtrk)>0.15) return 1;
+		else return 0;
+	}
+	else return 1;
+}
+
+int inputConfig::evtSelection(Long64_t jentry){
+		if ( tt->HBHENoiseFilterResultRun2Loose ==0) continue;
+		if ( tt->pcollisionEventSelection ==0) continue;
+		if ( tt->pprimaryVertexFilter ==0) continue;
+		if ( tt->phfCoincFilter3 ==0) continue;
+		if( abs(tt->vz) > 15) continue;
+}
+#endif
--- Toolkit/trkCorrTool.h
+++ Toolkit/trkCorrTool.h
@@ -48,22 +48,59 @@ void trkCorrTool::getCorrTable(TString svname, const int opt){
 						ptBinTitle[j]+"<=pt<"+ptBinTitle[j+1]);
 				hreco[i][j][k]->SetTitle(Form("reco: cent in [%d,%d), ",centBin[i],centBin[i+1])+\
 						ptBinTitle[j]+"<=pt<"+ptBinTitle[j+1]);
-				hgen[i][j][k]->Sumw2();
+				hgen [i][j][k]->Sumw2();
 				hreco[i][j][k]->Sumw2();
 			}
 		}
 	}
 	int hi= -1, hj = -1;
-	float wvz = 1;
-	float wcent = 1;
+	//weight will be store in an array
+	// weight[0] = 1 //no weight at all;
+	// weight[1] = vzWeight;
+	// weight[2] = vzWeight* centWeight;
+	float weight[2];
+	weight[0]=1;
 	Long64_t nentries = _t->fChain->GetEntriesFast();
 	std::cout<<"started to filling!"<<std::endl;
 	for( Long64_t jentry=0; jentry < nentries; jentry++){
 		hi   =-1;
 	       	hj   =-1;
-		wvz  = 1;
-		wcent= 1;
 		if( inputConfig.evtSelection(jentry)) continue;
+		weight[1]=inputConfig.weight1(jentry);
+		weight[2]=weight[1]*inputConfig.weight2(jentry);
+		hi = inputConfig.centNumber(jentry);
+		if( hi < 0) continue;
+		// filling the gen particles--------------------------
+		for ( int j =0; j< _t->pt->size(); j++){
+			if(_t->chg->at(j) ==0) continue;
+			hj = -1;
+			for ( int k = 0; k< nptbin; k++) if(_t->pt->at(j) >= ptBin[k] && _t->pt->at(j) < ptBin[k+1]) {
+				hj = k;
+				break;
+			}
+			if ( hj <0) continue;
+			for(int k=0;k<opt;k++){
+				hgen[hi][hj][k]->Fill(_t->eta->at(j),_t->phi->at(j),weight[k]);
+			}
+		}
+		//filling the reco ---------------------------------------
+		for ( int j =0; j< t->trkPt->size(); j++){
+			hj = -1;
+			if(trkQualityCuts(j)) continue;
+			for(int k=0;k<opt;k++){
+				hreco[hi][hj][k]->Fill(_t->trkEta->at(j),_t->trkPhi->at(j),weight[k]);
+			}
+	}
+	TFile *wf = TFile::Open(svname,"recreate");
+	for( int i =0; i<ncentBin; i++){
+		for( int j =0; j<nptBin; j++){
+			for ( int k = 0; k< opt; k++){
+				hgen[i][j][k]->Write();
+				hreco[i][j][k]->Write();
+				}
+		}
+	}
+	return;
 }
 
 #endif
