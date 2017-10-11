c84e546b1f5d1b83a1da497688fcd5016c4220a6
Tabris <tabris.xiao@gmail.edu>
2017-10-11 18:53:49 -0500

    check path for tracking

---
+++ iJTC/lib/trackingClosure.h
@@ -0,0 +1,411 @@
+
+#ifndef config_H
+#include "config.h"
+#endif
+
+#ifndef inputTree_H
+#include "inputTree.h"
+#endif
+#ifndef dataTree_H
+#include "dataTree.h"
+#endif
+
+#ifndef xthf4_H
+#include "xthf4.h"
+#endif
+
+#ifndef ROOT_TCanvas
+#include "TCanvas.h"
+#endif
+
+#ifndef ROOT_TLegend
+#include "TLegend.h"
+#endif
+
+#ifndef ROOT_TLine
+#include "TLine.h"
+#endif
+
+#ifndef xUtility
+#include "utility.h"
+#endif
+
+#ifndef trackingClosure_H
+#define trackingClosure_H
+using namespace jetTrack;
+
+class trackingClosure  {
+	public: 
+		trackingClosure(TString file):
+			ntrkpt(trackingClosureConfig::ntrkpt),
+			ncent(trackingClosureConfig::ncent),
+			nx(trackingClosureConfig::neta),
+			ny(trackingClosureConfig::nphi),
+			etamin(trackingClosureConfig::etamin),
+			etamax(trackingClosureConfig::etamax),
+			phimin(trackingClosureConfig::phimin),
+			phimax(trackingClosureConfig::phimax),
+			trkpt(trackingClosureConfig::trkpt),
+			cent(trackingClosureConfig::cent)
+	{
+		i_t=NULL;
+		d_t=NULL;
+		isread = 1;
+		gen = new xthf4();
+		rec = new xthf4();
+		cre = new xthf4();
+		f= TFile::Open(file);
+	};
+		trackingClosure(inputTree* tt):
+			i_t(tt),
+			ntrkpt(trackingClosureConfig::ntrkpt),
+			ncent(trackingClosureConfig::ncent),
+			nx(trackingClosureConfig::neta),
+			ny(trackingClosureConfig::nphi),
+			etamin(trackingClosureConfig::etamin),
+			etamax(trackingClosureConfig::etamax),
+			phimin(trackingClosureConfig::phimin),
+			phimax(trackingClosureConfig::phimax),
+			trkpt(trackingClosureConfig::trkpt),
+			cent(trackingClosureConfig::cent)
+	{
+		int   npt  = trackingClosureConfig::npt;
+		float ptmin= trackingClosureConfig::ptmin;
+		float ptmax= trackingClosureConfig::ptmax;
+		int   ncent_pt   =trackingClosureConfig::ncent_pt;
+		Double_t* cent_pt=trackingClosureConfig::cent_pt;
+		d_t=NULL;
+		f=NULL;
+		isread = 0;
+		gen = new xthf4("gen","gen", nx,etamin, etamax , ny, phimin, phimax,ntrkpt, trkpt,ncent, cent);
+		rec = new xthf4("rec","rec", nx,etamin, etamax , ny, phimin, phimax,ntrkpt, trkpt,ncent, cent);
+		cre = new xthf4("cre","cre", nx,etamin, etamax , ny, phimin, phimax,ntrkpt, trkpt,ncent, cent);
+		genpt= new TH2F("genpt", "gen pt distriubtion"        ,npt, ptmin, ptmax, ncent_pt, cent_pt);
+		recpt= new TH2F("recpt", "track pt distriubtion"      ,npt, ptmin, ptmax, ncent_pt, cent_pt);
+		crept= new TH2F("crept", "corr. track pt distriubtion",npt, ptmin, ptmax, ncent_pt, cent_pt);
+		genpt->Sumw2();
+		recpt->Sumw2();
+		crept->Sumw2();
+	};
+
+		trackingClosure(dataTree* tt):
+			d_t(tt),
+			ntrkpt(trackingClosureConfig::ntrkpt),
+			ncent(trackingClosureConfig::ncent),
+			nx(trackingClosureConfig::neta),
+			ny(trackingClosureConfig::nphi),
+			etamin(trackingClosureConfig::etamin),
+			etamax(trackingClosureConfig::etamax),
+			phimin(trackingClosureConfig::phimin),
+			phimax(trackingClosureConfig::phimax),
+			trkpt(trackingClosureConfig::trkpt),
+			cent(trackingClosureConfig::cent)
+	{
+		int   npt  = trackingClosureConfig::npt;
+		float ptmin= trackingClosureConfig::ptmin;
+		float ptmax= trackingClosureConfig::ptmax;
+		int   ncent_pt   =trackingClosureConfig::ncent_pt;
+		Double_t* cent_pt=trackingClosureConfig::cent_pt;
+		f=NULL;
+		i_t=NULL;
+		isread = 0;
+		rec = new xthf4("rec","rec", nx,etamin, etamax , ny, phimin, phimax,ntrkpt, trkpt,ncent, cent);
+		cre = new xthf4("cre","cre", nx,etamin, etamax , ny, phimin, phimax,ntrkpt, trkpt,ncent, cent);
+		recpt= new TH2F("recpt", "track pt distriubtion"      ,npt, ptmin, ptmax, ncent_pt, cent_pt);
+		crept= new TH2F("crept", "corr. track pt distriubtion",npt, ptmin, ptmax, ncent_pt, cent_pt);
+		recpt->Sumw2();
+		crept->Sumw2();
+	};
+
+		void runScan();
+		void runMCScan(inputTree* t);
+		void runDataScan(dataTree *t);
+		void Read();
+		void DrawClosure(TString name="", TString type= "");
+		TPad* ratioPlot(TH1F* hden, TH1F* hnum1, TH1F* hnum2, 
+				float xmin, float xmax,float ymin,float ymax, bool doLegend, bool dofit=0);
+		~trackingClosure();
+
+	public: 
+		TFile *f;
+		inputTree * i_t;
+		dataTree * d_t;
+		int ntrkpt;
+		int ncent;
+		int nx, ny;
+		float *trkpt, *cent;
+		float etamin, etamax, phimin, phimax;
+		xthf4 * gen;
+		xthf4 * rec;
+		xthf4 * cre;
+		TH2F * genpt;
+		TH2F * recpt;
+		TH2F * crept;
+		bool isread;
+		std::vector<TH1F*> ratioVec;
+};
+
+void trackingClosure::runScan(){
+	if(d_t ==NULL) runMCScan(i_t);
+	else runDataScan(d_t);
+}
+
+void trackingClosure::runMCScan(inputTree *t){
+	std::cout<<"prcessing the MC tree.."<<std::endl;
+	loadConfig();
+	Long64_t nentries = t->fChain->GetEntriesFast();
+	for(Long64_t jentry = 0; jentry <nentries ; ++jentry){
+		if(jentry %1000 == 0) std::cout<<"processing "<<jentry<<"th event.."<<std::endl;
+		t->GetEntry(jentry);
+		float w_vz=1;
+		float w_cent=1;
+		if(eventCuts(t)) continue;
+		w_vz = weight::vz_weight(t->vz);
+		w_cent = weight::cent_weight(t->hiBin);
+		for(int j=0;j<t->trkPt->size(); ++j){
+			if(trackQualityCuts(t,j))continue;
+			float trkcorr = correction::trk_corr(t, j);
+			rec->Fill(t->trkEta->at(j), t->trkPhi->at(j), t->trkPt->at(j), t->hiBin, w_vz);
+			cre->Fill(t->trkEta->at(j), t->trkPhi->at(j), t->trkPt->at(j), t->hiBin, w_vz*trkcorr);
+			recpt->Fill(t->trkPt->at(j), t->hiBin, w_cent*w_vz);
+			crept->Fill(t->trkPt->at(j), t->hiBin, w_cent*w_vz*trkcorr);
+		}
+			for(int j=0;j<t->pt->size(); ++j){
+				if(genParticleCuts(t,j))continue;
+				gen->Fill(t->eta->at(j), t->phi->at(j), t->pt->at(j), t->hiBin, w_vz);
+				genpt->Fill(t->pt->at(j), t->hiBin, w_cent*w_vz);
+			}
+	}
+	genpt->Scale(1.0/nentries);
+	recpt->Scale(1.0/nentries);
+	crept->Scale(1.0/nentries);
+	gen->Write();
+	rec->Write();
+	cre->Write();
+	genpt->Write();
+	recpt->Write();
+	crept->Write();
+}
+
+void trackingClosure::runDataScan(dataTree* t){
+	std::cout<<"prcessing the data tree.."<<std::endl;
+	loadConfig();
+	Long64_t nentries = t->fChain->GetEntriesFast();
+	for(Long64_t jentry = 0; jentry <nentries ; ++jentry){
+		if(jentry %1000 == 0) std::cout<<"processing "<<jentry<<"th event.."<<std::endl;
+		t->GetEntry(jentry);
+		float w_vz=1;
+		float w_cent=1;
+		if(eventCuts(t)) continue;
+		for(int j=0;j<t->trkPt->size(); ++j){
+			if(trackQualityCuts(t,j))continue;
+			float trkcorr = correction::trk_corr(t, j);
+			rec->Fill(t->trkEta->at(j), t->trkPhi->at(j), t->trkPt->at(j), t->hiBin);
+			cre->Fill(t->trkEta->at(j), t->trkPhi->at(j), t->trkPt->at(j), t->hiBin, trkcorr);
+			recpt->Fill(t->trkPt->at(j), t->hiBin);
+			crept->Fill(t->trkPt->at(j), t->hiBin, trkcorr);
+		}
+	}
+	recpt->Scale(1.0/nentries);
+	crept->Scale(1.0/nentries);
+	rec->Write();
+	cre->Write();
+	recpt->Write();
+	crept->Write();
+}
+
+void trackingClosure::Read(){
+	if(f==NULL) {
+		std::cout<<"trackingClosure contained histograms, can't read anymore"<<std::endl;
+		return ;
+	}
+	int ana_ntrkpt=anaConfig::ntrkpt;
+	int ana_ncent=anaConfig::ncent;
+	float* ana_trkpt = anaConfig::trkpt;
+	float* ana_cent = anaConfig::cent;
+	TFile* wf = TFile::Open("closure_Output.root", "recreate");
+	gen->Read("gen", "gen", f, ntrkpt, trkpt, ncent, cent);
+	rec->Read("rec", "rec", f, ntrkpt, trkpt, ncent, cent);
+	cre->Read("cre", "cre", f, ntrkpt, trkpt, ncent, cent);
+	gen->RebinZ(ana_ntrkpt, ana_trkpt);
+	rec->RebinZ(ana_ntrkpt, ana_trkpt);
+	cre->RebinZ(ana_ntrkpt, ana_trkpt);
+	gen->RebinW(ana_ncent, ana_cent);
+	rec->RebinW(ana_ncent, ana_cent);
+	cre->RebinW(ana_ncent, ana_cent);
+	genpt = (TH2F*) f->Get("genpt");
+	recpt = (TH2F*) f->Get("recpt");
+	crept = (TH2F*) f->Get("crept");
+	gen->Write();
+	rec->Write();
+	cre->Write();
+}
+
+trackingClosure::~trackingClosure(){
+	delete gen;
+	delete rec; 
+	delete cre; 
+	for(auto &it : ratioVec) delete it;
+	ratioVec.clear();
+}
+
+void trackingClosure::DrawClosure(TString name, TString type){
+	float xmin =-2.7 , xmax=2.7 , ymin = 0.0, ymax= 2;
+	int ana_ntrkpt=anaConfig::ntrkpt;
+	int ana_ncent=anaConfig::ncent;
+	float* ana_trkpt = anaConfig::trkpt;
+	float* ana_cent = anaConfig::cent;
+	ratioVec.reserve(ana_ntrkpt*ana_ncent);
+	TCanvas * c[ana_ntrkpt][2];
+	TH1F *hgen, *hrec, *hcre;
+	TLine *ll = new TLine();
+	ll->SetLineStyle(8);
+
+
+	auto ut= new padPolisher();
+	ut->tx->SetTextSize(0.07);
+	TPad* pd;
+	float fitRange0 = xmin, fitRange1 = xmax;
+	TF1* p0 = new TF1("p0", "pol0", fitRange0, fitRange1);
+	for(int i=0;i<ana_ntrkpt ; ++i){
+		c[i][0]= new TCanvas(Form("c_%d_0",i),"", 1200,600);
+		c[i][0]->Divide(4,1,0,0);
+		for(int j=0;j<ana_ncent; ++j){
+			c[i][0]->cd(ana_ncent-j);
+			hgen= (TH1F*)gen->hist(i,j)->ProjectionX();	
+			hrec= (TH1F*)rec->hist(i,j)->ProjectionX();	
+			hcre= (TH1F*)cre->hist(i,j)->ProjectionX();	
+			hgen->Rebin();
+			hrec->Rebin(); hcre->Rebin();
+			hcre->GetXaxis()->SetTitle("#eta_{track}");
+			pd=ratioPlot(hgen, hcre, hrec,xmin, xmax, ymin, ymax, !(ana_ncent-1-j), 1);
+			ll->DrawLine(xmin, 1, xmax,1);
+			ll->DrawLine(xmin, 1.05, xmax,1.05);
+			ll->DrawLine(xmin, .95, xmax,.95);
+			if(j==2){
+				pd->cd(2);
+				ut->ptLabel(i,ana_trkpt);
+			}
+			pd->cd(1);
+			ut->centLabel(j);
+		}		
+		if(name !="") c[i][0]->SaveAs("TrackingEfficiency_Eta_"+name+Form("_%d",i)+"."+type);
+	}
+	xmin =-3.2 , xmax=3.2 , ymin = 0.0, ymax= 2;
+	for(int i=0;i<ana_ntrkpt ; ++i){
+		c[i][1]= new TCanvas(Form("c_%d_1",i),"", 1200,600);
+		c[i][1]->Divide(4,1,0,0);
+		for(int j=0;j<ana_ncent; ++j){
+			c[i][1]->cd(ana_ncent-j);
+			hgen= (TH1F*)gen->hist(i,j)->ProjectionY();	
+			hrec= (TH1F*)rec->hist(i,j)->ProjectionY();	
+			hcre= (TH1F*)cre->hist(i,j)->ProjectionY();	
+			hgen->Rebin();
+			hrec->Rebin();
+			hcre->Rebin();
+			hcre->GetXaxis()->SetTitle("#phi_{track}");
+			pd=ratioPlot(hgen, hcre, hrec,xmin, xmax, ymin, ymax, !(ana_ncent-1-j));
+			ll->DrawLine(xmin, 1, xmax,1);
+			ll->DrawLine(xmin, 1.05, xmax,1.05);
+			ll->DrawLine(xmin, .95, xmax,.95);
+			if(j==2){
+				pd->cd(2);
+				ut->ptLabel(i,ana_trkpt);
+			}
+			pd->cd(1);
+			ut->centLabel(j);
+		}		
+		//ut->cent4Labels(c[i][1]);
+		if(name !="") c[i][1]->SaveAs("TrackingEfficiency_Phi_"+name+Form("_%d",i)+"."+type);
+	}
+
+	TH1F* closure[4];
+	if( name =="" || ana_ncent>4) return;
+	TCanvas *cpt = new TCanvas("cpt", "", 1200,340);
+	cpt->Divide(ana_ncent,1,0,0);
+	for(int ic=0; ic<ana_ncent; ++ic){
+		cpt->cd(ana_ncent-ic);
+		std::stringstream stream;
+		stream<<"cent : "<<fixed<<setprecision(0)<<ana_cent[ic]/2<<"-"<<fixed<<setprecision(0)<<ana_cent[ic+1]/2<<"%";
+		TString title = stream.str();
+		closure[ic]= new TH1F(Form("closure_pt_%d",ic),title , ana_ntrkpt, ana_trkpt);
+		for(int i=0;i<ana_ntrkpt;++i){
+			cout<<i+ic*ana_ncent<<endl;
+			ratioVec.at(i+ic*ana_ncent)->Fit(p0, "", "", fitRange0, fitRange1);
+			closure[ic]->SetBinContent(i+1, p0->GetParameter(0));
+			closure[ic]->SetBinError(i+1, p0->GetParError(0));
+		}
+		closure[ic]->Draw();
+		gPad->SetLogx();
+	}
+	cpt->SaveAs(name+"_closure_pt"+"."+type);
+	return;
+}
+
+TPad* trackingClosure::ratioPlot(TH1F* hden, TH1F* hnum1, TH1F* hnum2, 
+		float xmin, float xmax,float ymin,float ymax, bool doLegend, bool dofit)
+{
+	TH1F* hratio1, *hratio2;
+	hden->SetAxisRange(0, 1.8*hden->GetMaximum(), "y");
+	hden->SetMarkerSize(1.4);
+	hden->SetMarkerStyle(21);
+	hden->SetMarkerColor(kBlue-5);
+	hden->SetLabelSize(0.05, "Y");
+	hden->SetLabelSize(0.05, "X");
+	hnum1->SetMarkerSize(0.9);
+	hnum1->SetMarkerStyle(20);
+	hnum1->SetMarkerColor(kRed+1);
+	hnum1->SetTitle("");
+	hnum1->SetLabelSize(0.05, "Y");
+	hnum1->SetLabelSize(0.05, "X");
+	hnum1->GetXaxis()->SetTitleOffset(0.8);
+	hnum1->GetXaxis()->CenterTitle();
+	hnum1->GetXaxis()->SetTitleSize(0.06);
+	hratio1=(TH1F*)hnum1->Clone();
+	hden->SetAxisRange(xmin, xmax, "X");
+	hratio1->SetAxisRange(xmin, xmax, "X");
+	hratio1->Divide(hnum1, hden, 1, 1, "B");
+	hnum2->SetMarkerSize(0.9);
+	hnum2->SetMarkerStyle(20);
+	hnum2->SetTitle("");
+	hnum2->SetMarkerColor(kBlue-3);
+	hratio2=(TH1F*)hnum2->Clone();
+	hratio2->Divide(hnum2, hden, 1 ,1, "B");
+	TPad* pad = (TPad*) gPad;
+	pad->Divide(1, 2, 0,0);
+	pad->cd(1);
+	hden->SetStats(0);
+	hden->SetTitle("");
+	hden->Draw();
+	gPad->SetTicks(2,2);
+	hnum1->Draw("same");
+	hnum2->Draw("same");
+	TLegend *ltemp;
+	if(doLegend){
+		ltemp= new TLegend(0.15,0.67,0.6,0.97);
+		ltemp->SetLineColor(kWhite);
+		ltemp->SetFillColorAlpha(kWhite, 0);
+		ltemp->AddEntry(hden, "gen. particles");
+		ltemp->AddEntry(hnum1, "corr. tracks");
+		ltemp->AddEntry(hnum2, "reco. tracks");
+		ltemp->Draw();
+	}
+	pad->cd(2);
+	hratio1->SetStats(0);
+	hratio1->SetAxisRange(ymin, ymax , "Y");
+	hratio1->Draw();
+	hratio2->Draw("same");
+	gPad->SetTicks(2,2);
+	if(doLegend){
+		TLegend *ltemp2 = new TLegend(0.15,0.72,0.8,0.97);
+		ltemp2->SetLineColor(kWhite);
+		ltemp2->SetFillColorAlpha(kWhite, 0);
+		ltemp2->AddEntry(hratio1, "reco.Tracks/gen.Particles");
+		ltemp2->AddEntry(hratio2, "corr.Tracks/gen.Particles");
+		ltemp2->Draw();
+	}
+	ratioVec.push_back(hratio1);
+	return (TPad*)pad;
+}
+
+#endif
---
+++ iJTC/lib/trackingCorr.h
@@ -0,0 +1,332 @@
+
+#ifndef config_H
+#include "config_v1.h"
+#endif
+
+#ifndef inputTree_H
+#include "inputTree.h"
+#endif
+
+#ifndef xthf4_H
+#include "xthf4.h"
+#endif
+#ifndef trackingCorr_H
+#define trackingCorr_H
+
+#include "../Austin_TrkCorr/getTrkCorr.h"
+using namespace jetTrack;
+
+TrkCorr * Austin_corr = new TrkCorr("/Users/tabris/cmsProjects/iJTC/Austin_TrkCorr/");
+
+class trackingCorr  {
+	public: 
+		trackingCorr(inputTree* tt):
+			t(tt),
+			ntrkpt(trackingCorrConfig::ntrkpt_in),
+			ncent (trackingCorrConfig::ncent_in),
+			trkpt (trackingCorrConfig::trkpt_in),
+			cent  (trackingCorrConfig::cent_in),
+			nx    (trackingCorrConfig::neta),
+			ny    (trackingCorrConfig::nphi),
+			xmin  (trackingCorrConfig::etamin),
+			xmax  (trackingCorrConfig::etamax),
+			ymin  (trackingCorrConfig::phimin),
+			ymax  (trackingCorrConfig::phimax)
+	{
+		readonly =0;
+		corr=NULL;
+		gen = new xthf4("gen","gen", nx,xmin, xmax , ny, ymin, ymax,ntrkpt, trkpt,ncent, cent);
+		rec = new xthf4("rec","rec", nx,xmin, xmax , ny, ymin, ymax,ntrkpt, trkpt,ncent, cent);
+	};
+		trackingCorr(TFile *ff ):
+			ntrkpt(trackingCorrConfig::ntrkpt_in),
+			ncent (trackingCorrConfig::ncent_in),
+			trkpt (trackingCorrConfig::trkpt_in),
+			cent  (trackingCorrConfig::cent_in),
+			nx    (trackingCorrConfig::neta),
+			ny    (trackingCorrConfig::nphi),
+			xmin  (trackingCorrConfig::etamin),
+			xmax  (trackingCorrConfig::etamax),
+			ymin  (trackingCorrConfig::phimin),
+			ymax  (trackingCorrConfig::phimax),
+			f(ff)
+	{
+		readonly = 1;
+		corr=NULL;
+		gen = new xthf4();
+		rec = new xthf4();
+		doSymmetrization = false;
+	}
+
+		void runScan();
+		void Read();
+		void getCorr(TString file);
+		void showCorr(int i=-1, int j=-1);
+		void fixHole(TH2F* h);
+		void loadDataTracks(TString datafile);
+		void symmetrization(TH2F* h, TH2F* dh);
+		void symmetrization2(TH2F* h); // copy the right to the left
+		void patchCorr(TH2F* h, float pt, float cent);
+
+	public: 
+		inputTree * t;
+		int ntrkpt;
+		int ncent;
+		int nx, ny;
+		float *trkpt, *cent;
+		float xmin, xmax, ymin, ymax;
+		xthf4 * gen;
+		xthf4 * rec;
+		xthf4 * data_cre;
+		xthf4 * data_rec;
+		bool readonly;
+		TFile* f;
+		TFile* data_f;
+		TH2F** corr;
+		int ntrkpt_out  ;
+		int ncent_out   ;
+		float *trkpt_out;
+		float *cent_out ; 
+		bool doSymmetrization;
+
+};
+
+void trackingCorr::runScan(){
+	Long64_t nentries = t->fChain->GetEntriesFast();
+	for(Long64_t jentry = 0; jentry <nentries ; ++jentry){
+		t->GetEntry(jentry);
+		float w_vz=1;
+		if(eventCuts(t)) continue;
+		w_vz = weight::vz_weight(t->vz);
+		for(int j=0;j<t->trkPt->size(); ++j){
+			if(trackQualityCuts(t,j))continue;
+			rec->Fill(t->trkEta->at(j), t->trkPhi->at(j), t->trkPt->at(j), t->hiBin, w_vz);
+		}
+		for(int j=0;j<t->pt->size(); ++j){
+			if(genParticleCuts(t,j))continue;
+			gen->Fill(t->eta->at(j), t->phi->at(j), t->pt->at(j), t->hiBin, w_vz);
+		}
+	}
+	gen->Write();
+	rec->Write();
+}
+
+void trackingCorr::loadDataTracks(TString file){
+	doSymmetrization = true;
+	data_f = TFile::Open(file);
+	data_cre= new xthf4(); 
+	data_rec= new xthf4(); 
+	data_cre->Read("cre", "cre", data_f, ntrkpt, trkpt, ncent, cent);
+	data_rec->Read("rec", "rec", data_f, ntrkpt, trkpt, ncent, cent);
+}
+
+void trackingCorr::Read(){
+	if(!readonly) {
+		std::cout<<"already filled by obj, can't load anymore!"<<std::endl;
+		return ;
+	}
+	gen->Read("gen", "gen", f, ntrkpt, trkpt, ncent, cent);
+	rec->Read("rec", "rec", f, ntrkpt, trkpt, ncent, cent);
+	return ;
+}
+
+void trackingCorr::getCorr(TString file){
+
+	TFile* wf = TFile::Open(file, "recreate");
+	ntrkpt_out = trackingCorrConfig::ntrkpt_out;
+	trkpt_out = trackingCorrConfig::trkpt_out;
+	ncent_out = trackingCorrConfig::ncent_out;
+	cent_out = trackingCorrConfig::cent_out;
+
+	/*
+	//for gettign aux file to fix the hole problem
+	ncent_out = 4;
+	float fcent[5] = {0,20, 60, 100, 200};
+	cent_out = fcent;
+	*/
+
+	gen->RebinZ(ntrkpt_out, trkpt_out);
+	rec->RebinZ(ntrkpt_out, trkpt_out);
+	gen->RebinW(ncent_out, cent_out);
+	rec->RebinW(ncent_out, cent_out);
+
+	int nhist = ntrkpt_out*ncent_out;
+	corr= new TH2F*[nhist];
+	for(int i=0; i<ntrkpt_out; ++i){
+		for(int j=0; j<ncent_out; ++j){
+			corr[i+j*ntrkpt_out]= (TH2F*) gen->hist(i,j)->Clone(Form("corr_%d_%d",i,j));
+			corr[i+j*ntrkpt_out]->Divide( (TH2F*) rec->hist(i,j));
+			if(i<2) fixHole(corr[i+j*ntrkpt_out]);
+			corr[i+j*ntrkpt_out]->Write();
+		}
+	}
+	//here didn't delete the histogram so the potential memory leak is expected!! needs to fix
+	int nptSymm = 2;
+	
+/*
+	if(!doSymmetrization) return;
+	TFile* wf2 = TFile::Open("symmetrized_"+file, "recreate");
+	// symmetrization only apply to the 3 lowest pt bins
+	for(int i=0; i<ntrkpt_out; ++i){
+		for(int j=0; j<ncent_out; ++j){
+			corr[i+j*ntrkpt_out]= (TH2F*) gen->hist(i,j)->Clone(Form("corr_%d_%d",i,j));
+			corr[i+j*ntrkpt_out]->Divide( (TH2F*) rec->hist(i,j));
+			if(i<2) fixHole(corr[i+j*ntrkpt_out]);
+			if(i<nptSymm) symmetrization((TH2F*)data_cre->hist(i,j), corr[i+j*ntrkpt_out]);
+			corr[i+j*ntrkpt_out]->Write();
+		}
+	}
+*/
+	/*
+	TFile* wf2 = TFile::Open("eta_symmetry_"+file, "recreate");
+	for(int i=0; i<ntrkpt_out; ++i){
+		for(int j=0; j<ncent_out; ++j){
+			corr[i+j*ntrkpt_out]= (TH2F*) gen->hist(i,j)->Clone(Form("corr_%d_%d",i,j));
+			corr[i+j*ntrkpt_out]->Divide( (TH2F*) rec->hist(i,j));
+			if(i<2) fixHole(corr[i+j*ntrkpt_out]);
+			if(i<nptSymm) symmetrization2(corr[i+j*ntrkpt_out]);
+			corr[i+j*ntrkpt_out]->Write();
+		}
+	}
+	*/
+	TFile* wf3 = TFile::Open("patched_"+file, "recreate");
+
+	for(int i=0; i<ntrkpt_out; ++i){
+		for(int j=0; j<ncent_out; ++j){
+			corr[i+j*ntrkpt_out]= (TH2F*) gen->hist(i,j)->Clone(Form("corr_%d_%d",i,j));
+			corr[i+j*ntrkpt_out]->Divide( (TH2F*) rec->hist(i,j));
+			if(i<2) fixHole(corr[i+j*ntrkpt_out]);
+			if(i<nptSymm) patchCorr(corr[i+j*ntrkpt_out],(trkpt_out[i]+trkpt_out[i+1])/2, (cent_out[j]+cent_out[j+1])/2);
+			corr[i+j*ntrkpt_out]->Write();
+		}
+	}
+
+}
+
+void trackingCorr::showCorr(int ih, int jh){
+	int nc= ceil(float(ncent_out)/20);
+	if( corr==NULL){
+		std::cout<<"no correction loaded!"<<std::endl;
+		return;
+	}
+	TCanvas **cc;
+	//show in 4 by 5 subpads
+	if(jh==-1){
+		if(ih==-1){
+			cc= new TCanvas*[int(nc*ntrkpt_out)];
+			for(int j=0; j<ntrkpt_out; ++j){
+				for(int ic=0; ic<nc; ++ic){
+					cc[ic+j*nc]=new TCanvas(Form("ccorr_%d",ic+j*nc),"", 2000,1600);
+					cc[ic+j*nc]->Divide(5,4,0,0);
+					for(int k=0;k<ncent_out && k<20; ++k){
+						cc[ic+j*nc]->cd(k+1);
+						corr[j+k*ntrkpt_out]->Draw("colz");
+					}
+				}
+			}
+		}
+		else if(ih< ntrkpt_out) {
+			cc= new TCanvas*[nc];
+			for(int ic=0; ic<nc; ++ic){
+				cc[ic]=new TCanvas(Form("ccorr_%d",ic),"", 2000,1600);
+				cc[ic]->Divide(5,4,0,0);
+				for(int k=0;k<ncent_out && k<20; ++k){
+					cc[ic]->cd(k+1);
+					cout<<k<<endl;
+					corr[ih+k*ntrkpt_out]->Draw("colz");
+				}
+			}
+		}
+	}
+	else if(ih< ntrkpt_out && jh<ncent_out){
+		auto ccc= new TCanvas("cccc","", 600,500);
+		corr[ih+jh*ntrkpt_out]->Draw("colz");
+	}
+}
+
+//for fixing the hole in the tracker in 2015 PbPb data
+void trackingCorr::fixHole(TH2F* h){
+	int xh=36, yh=65;
+	int radius=3;
+	float peak=0;
+	int ip, jp;
+	for(int i=xh-radius; i<xh+radius+1;i++){
+		for(int j=yh-radius; j<yh+radius+1;j++){
+			//	if(h->GetBinContent(i,j)!=0) continue;
+			if(peak< h->GetBinContent(i,j)){
+				peak = h->GetBinContent(i,j);
+				ip = i;
+				jp = j;
+			}
+		}
+	}
+	int rr=2;
+	float boundary=float(h->GetBinContent(ip,jp+radius)+h->GetBinContent(ip,jp-radius)
+			+h->GetBinContent(ip+radius,jp)+h->GetBinContent(ip-radius,jp))/4;
+	for(int i=ip-radius; i<ip+radius+1;i++){
+		for(int j=jp-radius; j<jp+radius+1;j++){
+			//cout<<"!"<<i<<", "<<j<<endl;
+			if(h->GetBinContent(i,j)==0){
+				float bin = peak- (peak-boundary)/5*pow(pow((j-jp),2)+pow((i-ip),2),0.5);
+				if(bin<0){ 
+					cout<<"!"<<endl;
+					bin = boundary;
+				}
+				h->SetBinContent(i, j, bin);
+			}
+		}
+	}
+}
+
+void trackingCorr::symmetrization2(TH2F* h){
+	int eta_low=34, eta_up=41, phi_low=63, phi_up=67;
+	for(int i=eta_low; i<eta_up+1; ++i){
+		for(int j=phi_low; j<phi_up+1; ++j){
+			float content = h->GetBinContent(101-i, j);
+			cout<<" eta="<<h->GetXaxis()->GetBinCenter(i)<<" reflected eta="<<h->GetXaxis()->GetBinCenter(101-i)<<endl;
+//			cout<<content<<endl;
+			h->SetBinContent(i,j,content );
+		}
+	}
+}
+void trackingCorr::symmetrization(TH2F* h, TH2F* corr){
+	float cont ;
+	int nx = h->GetNbinsX();
+	int ny = h->GetNbinsY();
+	TH2F* temp = (TH2F*)h->Clone();
+	for(int j=0; j<ny+1;j++){
+		for(int i=1;i<nx+1;i++){
+//			if( j ==72)cout<<j<<endl;
+			if( float(ny)/2<j && h->GetBinContent(i,j) !=0){
+				cont = h->GetBinContent(i,j);
+				cont = h->GetBinContent(i,ny-j+1)/cont;
+				//                      if(cont == 0){
+				//                              cout<<"den: "<<h->GetBinContent(i,j)<<endl;
+				//                              cout<<"num: "<<h->GetBinContent(i,ny-j+1)<<" at x= "<<\
+				//                                      h->GetXaxis()->GetBinCenter(i)<<"; y= "<<\
+				//                                      h->GetYaxis()->GetBinCenter(ny-j+1)<<endl;
+				//                              cout<<"with i= "<<i<<"; "<<"j= "<<ny-j+1<<endl;
+				//                      }
+				temp->SetBinContent(i,j, cont);
+			}
+			//      else if( h->GetBinContent(i,j) !=0) temp->SetBinContent(i,j, 1);
+			else temp->SetBinContent(i,j, 1);
+		}
+	}
+	corr->Multiply(temp);
+	delete temp;
+}
+
+void trackingCorr::patchCorr(TH2F* h, float pt, float cent){
+	int eta_low=32, eta_up=43, phi_low=64, phi_up=67;
+	for(int i=eta_low; i<eta_up+1; ++i){
+		for(int j=phi_low; j<phi_up+1; ++j){
+			float eta=h->GetXaxis()->GetBinCenter(i);
+			float phi=h->GetYaxis()->GetBinCenter(j);
+			float content = Austin_corr->getTrkCorr(pt, eta, phi, cent, 999);
+			h->SetBinContent(i,j,content );
+		}
+	}
+}
+
+#endif
